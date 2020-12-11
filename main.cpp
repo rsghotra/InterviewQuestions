6f9fe52f2ca0e9329e5a8841ccf9c575f3e271f4
Rupinder Ghotra <ghotra.rupinder89@gmail.com>
2020-12-11 00:05:50 -0500

    Insert in a sorted array
    Change-Id: I38d963d0ce69e96064674ceb8fcf7c2b0dd7cff0

--- main.cpp
+++ main.cpp
@@ -48,13 +48,55 @@ void Display(vector<int> vect) {
     cout << endl;
 }
 
+void Separator(vector<int>& vect) {
+    int i = 0;
+    int j = vect.size() - 1;
+
+    while( i < j ) {
+        while(vect.at(i) < 0) {
+            i++;
+        }
+        while(vect.at(j) >= 0) {
+            j--;
+        }
+        //again checking because if in perfect array. Both will just increase and becomes i > j but the check will happen in next iteration
+        if(i < j) {
+            std::swap(vect.at(i), vect.at(j));
+        }
+    }
+}
+
+void Insert(vector<int>& vect, int data) {
+    int i = vect.size() - 1;
+    vect.push_back(0);
+    while(vect.at(i) >= data) {
+        vect.at(i) = vect.at(i-1);
+        i--;
+    }
+    vect[i+1] = data;
+}
+
 
 int main() {
-    vector<int> vect1{1,2,4,6,8,10,12,14};
+    vector<int> vect1{1,4,6,8,10,12,14};
     vector<int> vect2{1,3,5,7,9,11,13,15};
     
     isSorted(vect1) ? cout << "Vector is sorted" << endl : cout << "Vector is not sorted" << endl;
     vector<int> vect3 = MergeSorted(vect1, vect2);
     Display(vect3);
+
+    cout << "Inserting in a sorted array: " << endl;
+    Insert(vect3, 2);
+    Display(vect3);
+
+    vector<int> vect4{-6,3,-8,10,5,-7,-9,12,-4, 2};
+    Display(vect4);
+    cout << "Separating +ve and -ve numbers" << endl;
+    Separator(vect4);
+    Display(vect4);
+
+
+
+    return 0;
 }
 

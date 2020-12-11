7d6e357b80822aefd0bc6ca3efe6d2bb1139696c
Rupinder Ghotra <ghotra.rupinder89@gmail.com>
2020-12-10 19:47:17 -0500

    Search Transpose & Move Head
    Change-Id: I5832734c2366340fdf9a49b94c5dc46e4b6a6eac

--- main.cpp
+++ main.cpp
@@ -29,7 +29,69 @@ class Array {
         void Insert(const int&, const int&);
         void Append(const int&);
         int  Delete(const int&);
+        int  Get(const int&) const;
+        void Set(const int&, const int&) const;
+        int  Search(const int&) const;
+        int  Search_TRANSPOSE(const int& val);
+        int  Search_MOVE_HEAD(const int& val);
+        void Swap(int& x, int& y);
 };
+
+int Array::Search(const int& val) const {
+    if(this->length == 0) return -1;
+    for(int i{0}; i < this->length; i++) {
+        if(this->A[i] == val) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+void Array::Swap(int& x, int& y) {
+    int temp = x;
+    x = y;
+    y = temp;
+}
+
+int Array::Search_TRANSPOSE(const int& val) {
+    if(this->length == 0) return -1;
+    for(int i{0}; i < this->length; i++) {
+        if(this->A[i] == val) {
+            if(i != 0) {
+                this->Swap(this->A[i], this->A[i-1]);
+            }
+            return i;
+        }
+    }
+    return -1;
+}
+
+int Array::Search_MOVE_HEAD(const int& val) {
+    if(this->length == 0) return -1;
+    for(int i{0}; i < this->length; i++) {
+        if(this->A[i] == val) {
+            if(i!=0) {
+                this->Swap(this->A[0], this->A[i]);
+            }
+            return i;
+        }
+    }
+    return -1;
+}
+
+int Array::Get(const int& index) const{
+    if(index < 0 || index > this->length) {
+        throw invalid_argument("Invalid index.");
+    }
+    return this->A[index];
+}
+
+void Array::Set(const int& index, const int& data) const{
+    if(index < 0 || index > this->length - 1) {
+        throw out_of_range("Out of bound index is given.");
+    }
+    this->A[index] = data;
+}
 
 void Array::Display() const {
     if(this->length == 0) return;
@@ -92,12 +154,25 @@ int main() {
     arr.Insert(100, 101);
     arr.Display();
 
-    for(int i{0}; i < 101; i++) {
+    for(int i{0}; i < 50; i++) {
         cout << "Deleted: " << arr.Delete(i) << " ";
         if(i%5 == 0) cout << endl;
     }
     cout << endl;
-    arr.Append(1);
+   
     arr.Display();
+    cout << "Getting: " << arr.Get(10) << endl;
+    cout << "Setting: -99 at index 10" << endl;
+    arr.Set(10, -99);
+    cout << "Getting: " << arr.Get(10) << endl;
+
+    for(int i{0}; i < 15; i++) {
+        int val = randomInt(engine);
+        cout << "Searching(Transpose)for " <<  val << " and found at " << arr.Search_TRANSPOSE(val)<< endl;
+        arr.Display();
+        cout << "Searching for(Move Head) " <<  val << " and found at " << arr.Search_MOVE_HEAD(val)<< endl;
+        arr.Display();
+    }
+
     return 0;
 }

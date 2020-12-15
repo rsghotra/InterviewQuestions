942de0dbb8f340a4a875166d15a72611343994b5
Rupinder Ghotra <ghotra.rupinder89@gmail.com>
2020-12-14 23:49:33 -0500

    Delete from BST
    Change-Id: I82ffba741ec3a1dc7f50bd64fd7f531f33967b0d

--- main.cpp
+++ main.cpp
@@ -46,6 +46,69 @@ Node* Insert(Node* root, int data) {
     return root;
 }
 
+Node* InOrderPred(Node* ptr) {
+    while(ptr && ptr->right) {
+        ptr = ptr->right;
+    }
+    return ptr;
+}
+
+Node* InOrderSuc(Node* ptr) {
+    while(ptr && ptr->left) {
+        ptr = ptr->left;
+    }
+    return ptr;
+}
+
+int Height(Node* root) {
+    if(root == 0) {
+        return 0;
+    }
+    if(root->left == 0 && root->right == 0) {
+        return 0; //option if you want to keep the height as 1 for zeroth node
+    }
+    int left = Height(root->left);
+    int right = Height(root->right);
+    if(left > right) {
+        return left + 1;
+    } else {
+        return right + 1;
+    }
+}
+
+
+Node* Delete(Node* ptr, int data) {
+    Node* temp = 0;
+    if(ptr == 0) {
+        return nullptr;
+    }
+    if(ptr->data == data && ptr->left == 0 && ptr->right == 0) {
+        //deletion of the lead node;
+        if(ptr == root) {
+            root = 0;
+        }
+        delete ptr;
+        ptr = 0;
+        return nullptr;
+    } 
+    if(ptr->data > data) {
+        ptr->left = Delete(ptr->left, data);
+    } else if(ptr->data < data) {
+        ptr->right = Delete(ptr->right, data);
+    } else {
+        //value found
+        if(Height(ptr->left) > Height(ptr->right)) {
+            temp = InOrderPred(ptr->left);
+            ptr->data = temp->data;
+            ptr->left = Delete(ptr->left, ptr->data); //new data now
+        } else {
+            temp = InOrderSuc(ptr->right);
+            ptr->data = temp->data;
+            ptr->right = Delete(ptr->right, ptr->data);
+        }
+    }
+}
+
 void CreateBST() {
     root = Insert(root, 10);
     Insert(root, 20);
@@ -57,6 +120,8 @@ void CreateBST() {
 
 int main() {
     CreateBST();
+    LevelOrder();
+    Delete(root, 10);
     LevelOrder();
     return 0;
 }
